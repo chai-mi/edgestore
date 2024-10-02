82822edb13a2a32a5256e4b6401641ef37eae9fe
Caiminoyume <me@chaimi.cc>
2024-10-02 22:39:26 +0800

    update: 存储增强

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,38 @@ const storecolo = 'storecolo.edgestore.link'
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		const TargetColo = request.headers.get('Store-Colo')
-		if (!TargetColo) {
+		if (TargetColo) {
+			return cachestore(request, ctx)
+		}
+		return cacheResponse(request, env, ctx, async (request, env, ctx) => {
+			if (['PUT', 'DELETE'].includes(request.method))
+				ctx.waitUntil(cachestore(request.clone(), ctx))
 			return fetch(request, {
 				headers: { 'Store-Colo': storecolo },
 				cf: { resolveOverride: storecolo },
 			})
+		})
+	}
+}
+
+async function cacheResponse(
+	request: Request,
+	env: Env,
+	ctx: ExecutionContext,
+	getResponse: (request: Request, env: Env, ctx: ExecutionContext) => Promise<Response>
+) {
+	if (request.method === 'GET') {
+		const cacheRquest = new Request(request.url)
+		const response = await caches.default.match(cacheRquest)
+		if (response && response.status === 200) {
+			return response
+		} else {
+			const response = await getResponse(request, env, ctx)
+			ctx.waitUntil(caches.default.put(request, response.clone()))
+			return response
 		}
-		return cachestore(request, ctx)
+	} else {
+		return getResponse(request, env, ctx)
 	}
 }
 
