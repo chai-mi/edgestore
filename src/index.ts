e0e00d37ff8ea84437a3d95b8f168b378f7ab3c0
Caiminoyume <me@chaimi.cc>
2024-10-06 19:48:36 +0800

    fix: 修复服务端删除不正确的问题

--- src/index.ts
+++ src/index.ts
@@ -23,12 +23,7 @@ async function cachestore(request: Request, ctx: ExecutionContext): Promise<Resp
 	const cacheRquest = new Request(request.url)
 	switch (request.method) {
 		case 'PUT':
-			const cacheResponse = new Response(await request.bytes(), {
-				headers: {
-					'Cache-Control': 's-maxage=604800',
-					'Content-Type': 'application/octet-stream'
-				}
-			})
+			const cacheResponse = new Response(await request.bytes(), { headers: { 'Cache-Control': 's-maxage=604800' } })
 			ctx.waitUntil(caches.default.put(cacheRquest, cacheResponse))
 			return new Response(JSON.stringify({
 				url: request.url,
@@ -40,7 +35,12 @@ async function cachestore(request: Request, ctx: ExecutionContext): Promise<Resp
 				url: request.url
 			}), { status: deleted ? 200 : 500 })
 		case 'GET':
-			return await caches.default.match(cacheRquest) || new Response('Resource does not exist or has expired', { status: 404 })
+			const response = await caches.default.match(cacheRquest)
+			if (!response) {
+				return new Response('Resource does not exist or has expired', { status: 404 })
+			} else {
+				return new Response(response.body, { headers: { 'Content-Type': 'application/octet-stream' } })
+			}
 		default:
 			return new Response(null, { status: 400 })
 	}
